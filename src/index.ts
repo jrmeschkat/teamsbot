749b44a9a44b0ff4edc3f5c23a7e748c3536cc8a
Robert <robert.meschkat@googlemail.com>
2019-06-07 15:58:15 +0200

    added first testing funcionality

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+import { BotFrameworkAdapter, TurnContext } from 'botbuilder';
+import { EchoBot } from './bot';
 
 const app = express();
-
 const { PORT = 3000 } = process.env;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send({ message: 'Hello World!' });
+const echo = new EchoBot();
+
+const adapter = new BotFrameworkAdapter({
+  // appId: process.env.MICROSOFT_APP_ID,
+  // appPassword: process.env.MICROSOFT_APP_PASSWORD
+  appId: '20ad4a1c-0c4f-4a01-89f5-7021fad52bf7',
+  appPassword: 'bcS2TjUWKQE2LF[sA1B7TRq+JRzjaA:['
+});
+
+app.post('/api/messages', (req: Request, res: Response) => {
+  adapter.processActivity(req, res, async (context: TurnContext) => {
+    await echo.onTurn(context);
+  });
 });
 
 app.listen(PORT, () => {
