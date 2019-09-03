03d680dda50c6c07fb1258176c520b4f333086f6
Robert <robert.meschkat@googlemail.com>
2019-09-03 13:41:52 +0200

    wip

--- uniksbot/lib/app.ts
+++ uniksbot/lib/app.ts
@@ -1,13 +1,9 @@
 import { BotFrameworkAdapter } from 'botbuilder';
 import * as restify from 'restify';
 import { UniksBot } from './bot';
-import { QnAMaker, LuisRecognizer } from 'botbuilder-ai';
 import { config } from 'dotenv';
-import { IQnAService, BotConfiguration, ILuisService } from 'botframework-config';
 
 config();
-
-const botConfig = BotConfiguration.loadSync('./jrmbot.bot', process.env.BOT_FILE_SECRET);
 
 let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
@@ -19,23 +15,7 @@ const adapter = new BotFrameworkAdapter({
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-const qnaConfig = botConfig.findServiceByNameOrId('qna') as IQnAService;
-
-const qnaMaker: QnAMaker = new QnAMaker({
-  knowledgeBaseId: qnaConfig.kbId,
-  endpointKey: qnaConfig.endpointKey,
-  host: qnaConfig.hostname
-});
-
-const luisConfig = botConfig.findServiceByNameOrId('luis') as ILuisService;
-
-const luis = new LuisRecognizer({
-  applicationId: luisConfig.appId,
-  endpointKey: luisConfig.subscriptionKey,
-  endpoint: luisConfig.getEndpoint()
-});
-
-const echo: UniksBot = new UniksBot(qnaMaker, luis);
+const echo: UniksBot = new UniksBot();
 
 server.post('/api/messages', (req, res) => {
   adapter.processActivity(req, res, async context => {
--- uniksbot/lib/bot.ts
+++ uniksbot/lib/bot.ts
@@ -1,44 +1,9 @@
-import { TurnContext } from 'botbuilder';
-import { QnAMaker, LuisRecognizer } from 'botbuilder-ai';
+import { TurnContext, ActivityTypes } from 'botbuilder';
 
 export class UniksBot {
-  private qnaMaker: QnAMaker;
-  private luis: LuisRecognizer;
-
-  constructor(qnaMaker: QnAMaker, luis: LuisRecognizer) {
-    this.qnaMaker = qnaMaker;
-    this.luis = luis;
-  }
-
   async onTurn(context: TurnContext) {
-    if (context.activity.type === 'message') {
-      let qnaResults = [];
-      // qnaResults = await this.qnaMaker.generateAnswer(context.activity.text);
-      if (qnaResults.length > 0) {
-        await context.sendActivity(qnaResults[0].answer);
-      } else {
-        await this.luis
-          .recognize(context)
-          .then(res => {
-            const top = LuisRecognizer.topIntent(res);
-            console.log(top + '\t ' + res.entities);
-            switch (top) {
-              case 'Name':
-                const firstname = res.entities.firstname;
-                const lastname = res.entities.lastname;
-                context.sendActivity(`Hello ${firstname ? firstname : ''} ${lastname ? lastname : ''}`);
-                break;
-
-              default:
-                context.sendActivity("Sorry, I didn't understood that.");
-                break;
-            }
-          })
-          .catch(console.error);
-        // await
-      }
-    } else {
-      await context.sendActivity(`${context.activity.type} event detected`);
+    if (context.activity.type === ActivityTypes.Message) {
+      context.sendActivity(`You said: ${context.activity.text}`);
     }
   }
 }
